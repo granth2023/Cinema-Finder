26fca818a0afa09615af8a7375ff660640d37b67
granth2023 <grantharris38@gmail.com>
2023-09-01 12:59:32 -0400

    adding api

--- src/pages/SearchMovie.tsx
+++ src/pages/SearchMovie.tsx
@@ -1,39 +1,47 @@
 import React, { useState } from 'react';
 
-const SearchMoive = () => {
-    const [query, setQuery] = useState('');
-    const [movie, setMovie] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+const SearchMovie = () => {
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const fetchMovie =  async () => {
-        setLoading(true);
-        setError(null);
+  const fetchMovie = async () => {
+    setLoading(true);
+    setError(null);
 
-        try {
-            const res = await fetch('/api/searchMovie?query=${query}');
-            if(!res.ok) {
-                throw Error('Something went wrong');
-            }
-            const data = await res.json();
-            setMovie(data);
-        } catch (err) {
-            setError(err.message);
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const res = await fetch(`/api/searchMovie?query=${query}`);
+      if (!res.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await res.json();
+      setMovie(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return ( 
-        <div> 
-            <input type="text" onChange={(e) => setQuery(e.target.value)} />
-            <button onClick={fetch}>Search</button>
-
-            {loading && <p>Loading...</p>}
-
-            {error && <p>Error: {error}</p>}
-
-            
+  return (
+    <div>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <button onClick={fetchMovie}>Search</button>
+      
+      {loading && <p>Loading...</p>}
+      
+      {error && <p>Error: {error}</p>}
+      
+      {movie && !loading && !error && (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.poster} alt={movie.title} />
+          {/* Display other movie details here */}
         </div>
+      )}
+    </div>
+  );
+};
 
-    )
+export default SearchMovie;
