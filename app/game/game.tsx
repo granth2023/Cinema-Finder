a7e9eb77924bafbc5a78d0131534703449358b4b
granth2023 <grantharris38@gmail.com>
2024-01-22 14:06:29 -0500

    gameboard component added to game page

--- app/game/game.tsx
+++ app/game/game.tsx
@@ -7,7 +7,7 @@ const GamePage: React.FC = () => {
     return (
         <div className="container mx-auto p-4"> 
             <h1 className= "text-3x1 font-bold mb-4"> Memory Game</h1>
-        
+            <GameBoard />
         </div>
     )
 }
