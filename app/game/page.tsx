2c6f349f1b1f58b6306df62df25a4efbb29efab4
granth2023 <grantharris38@gmail.com>
2024-01-21 20:43:06 -0500

    server side props passed in page tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,13 +2,30 @@ import React from 'react';
 
 import GameBoard from './components/GameBoard';
 
+export async function getServerSideProps(context){
+    const gameData =await fetchGameData();
+
+    return {
+        props: {
+            gameData,
+        },
+    };
+}
+
 const GamePage: React.FC = () => {
     return (
         <div>
             <h1>Memory Game</h1>
-            <GameBoard />
+            <GameBoard initalData={gameData} />
         </div>
     )
 
 }
 export default GamePage;
+
+async function fetchGameData(){
+
+    return{
+
+    } 
+}
