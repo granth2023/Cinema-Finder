96de4c109a044d944ff6b1c30eeabca2d04bd6dd
granth2023 <grantharris38@gmail.com>
2024-01-22 14:06:14 -0500

    memory game h1 class created

--- app/game/game.tsx
+++ app/game/game.tsx
@@ -6,7 +6,7 @@ const GameBoard = dynamic(() => import('./GameBoard'), {ssr: false})
 const GamePage: React.FC = () => {
     return (
         <div className="container mx-auto p-4"> 
-        
+            <h1 className= "text-3x1 font-bold mb-4"> Memory Game</h1>
         
         </div>
     )
